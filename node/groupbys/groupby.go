7b071d9193cbcb86c79d7ef6ef346730a1930266
rafaortegano <rortegano@fi.uba.ar>
2025-09-30 17:15:54 -0300

    add: RecordGroupBy

---
+++ node/groupbys/groupby.go
@@ -0,0 +1,12 @@
+package groupbys
+
+import (
+	"github.com/distribuidos-Coffee-Shop-Analysis/nodes/protocol"
+)
+
+// RecordGroupBy defines the interface for different types of groupby operations
+type RecordGroupBy interface {
+	ProcessBatch(records []protocol.Record, eof bool) ([]protocol.Record, error)
+	Name() string
+	Reset()
+}

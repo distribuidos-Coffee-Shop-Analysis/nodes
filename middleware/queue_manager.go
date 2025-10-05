eb186cd751ee7d56de1bb65479d702a929dfc05f
tinchomorilla <mmorilla@fi.uba.ar>
2025-10-05 18:35:50 -0300

    update message acknowledgment handling in StartConsuming method

--- middleware/queue_manager.go
+++ middleware/queue_manager.go
@@ -111,7 +111,7 @@ func (qm *QueueManager) StartConsuming(callback func(batch *protocol.BatchMessag
 	// Start consuming from each queue
 	for i := range qm.Wiring.Bindings {
 		var err error
-	
+
 		dedicatedChannel := qm.channel
 		// Determine queue name: single binding uses base name, multiple bindings add suffix
 		queueName := qm.Wiring.QueueName
@@ -152,17 +152,17 @@ func (qm *QueueManager) StartConsuming(callback func(batch *protocol.BatchMessag
 						batchMessage, err = protocol.BatchMessageFromData(msg.Body)
 						if err != nil {
 							log.Printf("action: parse_batch | queue: %s | result: fail | error: %v", qName, err)
-							msg.Nack(false, true) // Reject and requeue
+							msg.Ack(false) // Ack to remove bad message
 							continue
 						}
 					} else {
 						log.Printf("action: parse_message | queue: %s | result: fail | error: unknown message type: %d", qName, msgType)
-						msg.Nack(false, true)
+						msg.Ack(false) // Ack to remove bad message
 						continue
 					}
 				} else {
 					log.Printf("action: parse_message | queue: %s | result: fail | error: empty message body", qName)
-					msg.Nack(false, true)
+					msg.Ack(false) // Ack to remove bad message
 					continue
 				}
 
@@ -193,37 +193,6 @@ func (qm *QueueManager) StopConsuming() {
 
 // MessageMiddleware interface methods
 
-// Send sends message to default output exchange
-func (qm *QueueManager) Send(message []byte) error {
-	// Use the first available output exchange as default
-	var exchange, routingKey string
-	for _, route := range qm.Wiring.Outputs {
-		exchange = route.Exchange
-		routingKey = route.RoutingKey
-		break
-	}
-
-	if exchange == "" {
-		return fmt.Errorf("no output exchange configured")
-	}
-
-	err := qm.channel.Publish(
-		exchange,   // exchange
-		routingKey, // routing key
-		false,      // mandatory
-		false,      // immediate
-		amqp.Publishing{
-			DeliveryMode: amqp.Persistent, // Make message persistent
-			Body:         message,
-		})
-
-	if err != nil {
-		log.Printf("action: send | result: fail | error: %v", err)
-		return err
-	}
-	return nil
-}
-
 // Close closes the connection
 func (qm *QueueManager) Close() error {
 	qm.consuming = false
@@ -238,145 +207,3 @@ func (qm *QueueManager) Close() error {
 	}
 	return err
 }
-
-// DeleteExchanges deletes all declared exchanges
-func (qm *QueueManager) DeleteExchanges() error {
-	if qm.channel == nil {
-		return fmt.Errorf("no channel available")
-	}
-
-	// Delete all declared exchanges
-	for _, exchangeName := range qm.Wiring.DeclareExchs {
-		err := qm.channel.ExchangeDelete(exchangeName, false, false)
-		if err != nil {
-			log.Printf("action: delete | result: fail | exchange: %s | error: %v", exchangeName, err)
-			return err
-		}
-		log.Printf("action: delete | result: success | exchange: %s", exchangeName)
-	}
-
-	return nil
-}
-
-// RabbitMQMiddleware implements the MessageMiddleware interface using QueueManager
-type RabbitMQMiddleware struct{}
-
-// NewRabbitMQMiddleware creates a new instance of RabbitMQMiddleware
-func NewRabbitMQMiddleware() *RabbitMQMiddleware {
-	return &RabbitMQMiddleware{}
-}
-
-// StartConsuming implements MessageMiddleware interface
-func (rmq *RabbitMQMiddleware) StartConsuming(m *QueueManager, onMessageCallback onMessageCallback) MessageMiddlewareError {
-	if m.Connection == nil || m.Connection.IsClosed() {
-		log.Printf("action: start_consuming | result: fail | error: no connection available")
-		return MessageMiddlewareDisconnectedError
-	}
-
-	msgs, err := m.channel.Consume(m.Wiring.QueueName, "", false, false, false, false, nil)
-	if err != nil {
-		log.Printf("action: start_consuming | result: fail | error: %v", err)
-		return MessageMiddlewareMessageError
-	}
-
-	m.consuming = true
-	log.Printf("action: start_consuming | result: success | queue: %s", m.Wiring.QueueName)
-
-	// Create a channel for the messages and start the callback in a goroutine
-	consumeChannel := ConsumeChannel(&msgs)
-	done := make(chan error, 1)
-
-	go onMessageCallback(consumeChannel, done)
-
-	// Wait for completion or error
-	err = <-done
-	if err != nil {
-		log.Printf("action: start_consuming | result: fail | error: %v", err)
-		return MessageMiddlewareMessageError
-	}
-
-	return 0 // No error
-}
-
-// StopConsuming implements MessageMiddleware interface
-func (rmq *RabbitMQMiddleware) StopConsuming(m *QueueManager) MessageMiddlewareError {
-	m.consuming = false
-	log.Println("action: stop_consuming | result: success")
-	return 0 // No error
-}
-
-// Send implements MessageMiddleware interface
-func (rmq *RabbitMQMiddleware) Send(m *QueueManager, message []byte) MessageMiddlewareError {
-	if m.Connection == nil || m.Connection.IsClosed() {
-		log.Printf("action: send | result: fail | error: no connection available")
-		return MessageMiddlewareDisconnectedError
-	}
-
-	// Use the first available output exchange as default
-	var exchange, routingKey string
-	for _, route := range m.Wiring.Outputs {
-		exchange = route.Exchange
-		routingKey = route.RoutingKey
-		break
-	}
-
-	if exchange == "" {
-		log.Printf("action: send | result: fail | error: no output exchange configured")
-		return MessageMiddlewareMessageError
-	}
-
-	err := m.channel.Publish(
-		exchange,   // exchange
-		routingKey, // routing key
-		false,      // mandatory
-		false,      // immediate
-		amqp.Publishing{
-			DeliveryMode: amqp.Persistent, // Make message persistent
-			Body:         message,
-		})
-
-	if err != nil {
-		log.Printf("action: send | result: fail | error: %v", err)
-		return MessageMiddlewareMessageError
-	}
-	return 0 // No error
-}
-
-// Close implements MessageMiddleware interface
-func (rmq *RabbitMQMiddleware) Close(m *QueueManager) MessageMiddlewareError {
-	m.consuming = false
-	var err error
-	if m.Connection != nil && !m.Connection.IsClosed() {
-		err = m.Connection.Close()
-	}
-	if err != nil {
-		log.Printf("action: close | result: fail | error: %v", err)
-		return MessageMiddlewareCloseError
-	} else {
-		log.Println("action: close | result: success")
-	}
-	return 0 // No error
-}
-
-// Delete implements MessageMiddleware interface
-func (rmq *RabbitMQMiddleware) Delete(m *QueueManager) MessageMiddlewareError {
-	if m.channel == nil {
-		log.Printf("action: delete | result: fail | error: no channel available")
-		return MessageMiddlewareDeleteError
-	}
-
-	// Delete all declared exchanges
-	for _, exchangeName := range m.Wiring.DeclareExchs {
-		err := m.channel.ExchangeDelete(exchangeName, false, false)
-		if err != nil {
-			log.Printf("action: delete | result: fail | exchange: %s | error: %v", exchangeName, err)
-			return MessageMiddlewareDeleteError
-		}
-		log.Printf("action: delete | result: success | exchange: %s", exchangeName)
-	}
-
-	return 0 // No error
-}
-
-// Ensure RabbitMQMiddleware implements MessageMiddleware interface
-var _ MessageMiddleware[QueueManager] = (*RabbitMQMiddleware)(nil)
